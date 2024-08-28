fb9b88f451c31807428400f170608fc3272cf6d0
farazalikhan0871 <faraz08710871@gmail.com>
2024-08-28 13:18:39 +0530

    setup commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
-    author_email= 'entbappy73@gmail.com',
+    author= 'FARAZ ALI KHAN',
+    author_email= '7411046755khan@gmail.com',
     packages= find_packages(),
     install_requires = []
 
